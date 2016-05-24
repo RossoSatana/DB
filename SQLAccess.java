77a4b81a4ba8295e38eaea81e922af66f43ddd20
MissVixen <le.morselli@gmail.com>
2016-05-24 11:44:47 +0200

    Add files via upload

--- SQLAccess.java
+++ SQLAccess.java
@@ -1,4 +1,4 @@
-package restlet.DB;
+package db.restlet;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,10 +10,9 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SQLAccess {
 	protected Connection connect = null;
@@ -23,19 +22,16 @@ public class SQLAccess {
 
 	public void connection() throws Exception {
 		try {
-			// This will load the MySQL driver, each DB has its own driver
 			Class.forName("com.mysql.jdbc.Driver");
-			// Setup the connection with the DB
+
 			connect = DriverManager
 					.getConnection("jdbc:mysql://localhost:3306/DBTESI?characterEncoding=UTF-8&useSSL=false",
 							"root", "moonlight3");
-
+					/*.getConnection("jdbc:mysql://localhost:3306/dbtesi?characterEncoding=UTF-8&useSSL=false",
+												"root", "root"); */
 		} catch (Exception e) {
 			throw e;
-		}/* finally {
-      close();
-    }*/
-
+		}
 	}
 
 	private String resultset_to_json(ResultSet rs) throws SQLException {
@@ -44,50 +40,16 @@ public class SQLAccess {
 			HashMap<String, String> row = new HashMap<String, String>();
 			for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) 
 				row.put(rs.getMetaData().getColumnName(i), String.valueOf(rs.getObject(i)));
-			jarr.add(new JSONObject(row));
+			jarr.put(new JSONObject(row));
 		}
 		return jarr.toString();
 	}
 
 	public String allUser ()  throws SQLException {
-		JSONObject obj=new JSONObject();
-		// Statements allow to issue SQL queries to the database
 		statement = connect.createStatement();
-		// Result set get the result of the SQL query
 		resultSet = statement.executeQuery("select * from USER");
-		String response="";
 
 		return resultset_to_json(resultSet);
-
-		/* while (resultSet.next()) {
-          // It is possible to get the columns via name
-          // also possible to get the columns via the column number
-          // which starts at 1
-          // e.g. resultSet.getSTring(2);
-    	  obj.put("user", resultSet.getString("ID"));
-    	  obj.put("pw", resultSet.getString("PW"));
-    	  obj.put("lvl", resultSet.getString("LVL"));
-      /*    Date date = resultSet.getDate("datum");  
-         response += obj.toJSONString();
-       }
-     // return response;
-   /*   preparedStatement = connect.prepareStatement("insert into USER (ID, PW) values ('Leti', 'M')");
-      preparedStatement.executeUpdate();
-      writeResultSet(resultSet);
-
-      preparedStatement = connect
-              .prepareStatement("insert into USER (ID, PW, LVL) values (?, ?, ?)");
-          // "myuser, webpage, datum, summary, COMMENTS from feedback.comments");
-          // Parameters start with 1
-          preparedStatement.setString(1, "Biagio");
-          preparedStatement.setString(2, "k");
-          preparedStatement.setString(3, "10");
-          preparedStatement.executeUpdate();
-
-      resultSet = statement
-      .executeQuery("select * from feedback.comments");
-      writeMetaData(resultSet); */
-
 	} 
 
 	public boolean checkUser (String user)  throws SQLException {
@@ -100,13 +62,51 @@ public class SQLAccess {
 		return true;		//user exist in DB 
 	} 
 
-	public String insertUser (String user, String pw)  throws SQLException {
-		// Statements allow to issue SQL queries to the database
+	public boolean checkOwner (String user, int COD_M) throws SQLException {
 		statement = connect.createStatement();
+		resultSet = statement.executeQuery("select * " +
+				"from USER u, MONSTER m" +
+				"where u.ID = " + "'" + user + "' " +
+				"and m.COD_M = " + COD_M +
+				"and u.ID = m.ID_OWNER");
 
+		if (!resultSet.next())
+			return false; 	//user isn't the owner of monster COD_M
+
+		return true;		//user is owner of COD_M 
+	} 
+	
+	public boolean checkOwner (String user, String w_name) throws SQLException {
+		statement = connect.createStatement();
+		resultSet = statement.executeQuery("select * " +
+				"from USER u, WEARABLE_OWNED w" +
+				"where u.ID = " + "'" + user + "' " +
+				"and w.W_NAME = " + "'" + w_name + "' " +
+				"and u.ID = w.ID_OWNER");
+
+		if (!resultSet.next())
+			return false; 	//user isn't the owner of item w_name
+
+		return true;		//user is owner of item w_name
+	} 
+	
+	public String findOwner (int COD_M) throws SQLException {		//return ID of the owner
+		statement = connect.createStatement();
+		resultSet = statement.executeQuery("select ID_OWNER " +
+				"from MONSTER_OWNED" +
+				"where COD_M = " + COD_M );
+
+		if (!resultSet.next())
+			return null; 	//user isn't the owner of item w_name
+
+		return resultSet.getString("ID_OWNER");		//user is owner of item w_name
+	} 
+	
+	public String insertUser (String user, String pw)  throws SQLException {
+		statement = connect.createStatement();
 		preparedStatement = connect
 				.prepareStatement("insert into USER (ID, PW, LVL, MANA) values (?, ?, 1, 10)");
-		// Parameters start with 1
+
 		preparedStatement.setString(1, user);
 		preparedStatement.setString(2, pw);
 		preparedStatement.executeUpdate();
@@ -124,33 +124,63 @@ public class SQLAccess {
 		String response = "You are now logged in as: " + user;
 		return response; 
 	}
-	
+
+	public String deleteUser (String user) throws SQLException { 
+
+		statement = connect.createStatement();
+		statement.executeUpdate("delete from USER where ID = " + "'" + user + "'");
+
+		String response = "User " + user + " has been deleted";
+		return response; 
+	}
+
+	public String equip (int COD_M, String W_NAME) throws SQLException { 
+		statement = connect.createStatement();
+		statement.executeUpdate("insert into from EQUIPPED (W_NAME, COD_M) values " +
+				"( " + "'" + W_NAME + "'" + ", " + COD_M);
+
+		String response = "Item " + W_NAME + " has been equipped from monster " + COD_M;
+		return response; 		
+	}
+
+	public String unequip (int COD_M, String w_name) throws SQLException { 
+
+		statement = connect.createStatement();
+		statement.executeUpdate("delete from EQUIPPED " +
+				"where W_NAME = " + "'" + w_name + "'" + 
+				"and COD_M = " + COD_M + ";" +
+				"set ");
+
+		String response = "Item " + w_name + " has been unequipped from monster " + COD_M;
+		return response; 
+	}
+
 	public String mInfo (String denomination) throws SQLException {
 		statement = connect.createStatement();
 		resultSet = statement.executeQuery(
 				"select * " +
 				"from MONSTER " +
 				"where  DENOMINATION = " + "'" + denomination + "'");
-		
+
 		String response;
 		response = resultset_to_json (resultSet);
 		return response;
 	}
-	
+
 	public String mInfo (int COD_M)	throws SQLException {
 		statement = connect.createStatement();
 		resultSet = statement.executeQuery(
 				"select * " +
 				"from MONSTER_OWNED " +
 				"where COD_M = " + COD_M );
-		
+
 		String response;
 		response = resultset_to_json (resultSet);
 		return response;
 	}
-	
+
 	public String mFighting (String user) throws SQLException { 
-		
+
 		if (checkUser(user) == false) {
 			return "{Error : User not found}";
 		}
@@ -161,14 +191,14 @@ public class SQLAccess {
 				"from MONSTER_FIGHTING mf, MONSTER_OWNED mo " +
 				"where  mo.ID_OWNER = " + "'" + user + "'" + " " +
 				"and mo.COD_M = mf.COD_M ");
-		
+
 		String response;
 		response = resultset_to_json (resultSet);	
 
 		return response; 
 	}
-	
-public String mAbility (int COD_M) throws SQLException { 
+
+	public String mAbility (int COD_M) throws SQLException { 
 
 		statement = connect.createStatement();
 		resultSet = statement.executeQuery(
@@ -176,73 +206,39 @@ public String mAbility (int COD_M) throws SQLException {
 				"from ABILITY a, MONSTER_ABILITY ma " +
 				"where  ma.COD_M = " + COD_M + " " +
 				"and ma.A_NAME = a.A_NAME ");
-		
+
 		String response;
 		response = resultset_to_json (resultSet);
-		
+
 		return response; 
 	}
 
+	public String mEquipped (int COD_M) throws SQLException { 
 
-public String mEquipped (int COD_M) throws SQLException { 
+		statement = connect.createStatement();
+		resultSet = statement.executeQuery(
+				"select * " +
+				"from EQUIPPED e, WEARABLE w " +
+				"where  e.COD_M = " + COD_M + " " +
+				"and e.W_NAME = w.W_NAME ");
 
-	statement = connect.createStatement();
-	resultSet = statement.executeQuery(
-			"select * " +
-			"from EQUIPPED e, WEARABLE w " +
-			"where  e.COD_M = " + COD_M + " " +
-			"and e.W_NAME = w.W_NAME ");
+		String response;
+		response = resultset_to_json (resultSet);
+
+		return response; 
+	}
 	
-	String response;
-	response = resultset_to_json (resultSet);
+	public String mCollection (String user) throws SQLException { 
+		statement = connect.createStatement();
+		resultSet = statement.executeQuery(
+	 		"select * " +
+			"from MONSTER_OWNED mo " +
+			"where  mo.ID_OWNER = '" + user + "'");
 	
-	return response; 
-}
+		String response;
+		response = resultset_to_json (resultSet);
 
-
-
-	private void writeMetaData(ResultSet resultSet) throws SQLException {
-		//   Now get some metadata from the database
-		// Result set get the result of the SQL query
-
-		System.out.println("The columns in the table are: ");
-
-		System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
-		for  (int i = 1; i<= resultSet.getMetaData().getColumnCount(); i++){
-			System.out.println("Column " +i  + " "+ resultSet.getMetaData().getColumnName(i));
-		}
-	}
-
-	private void writeResultSet(ResultSet resultSet) throws SQLException {
-		// ResultSet is initially before the first data set
-		while (resultSet.next()) {
-			// It is possible to get the columns via name
-			// also possible to get the columns via the column number
-			// which starts at 1
-			// e.g. resultSet.getSTring(2);
-			String user = resultSet.getString("ID");
-			/*    Date date = resultSet.getDate("datum");  */
-			System.out.println("User: " + user);
-		}
-	}
-
-	// You need to close the resultSet
-	private void close() {
-		try {
-			if (resultSet != null) {
-				resultSet.close();
-			}
-
-			if (statement != null) {
-				statement.close();
-			}
-
-			if (connect != null) {
-				connect.close();
-			}
-		} catch (Exception e) {
-
-		}
-	}
+  		return response; 
+  	}
 
 } 
